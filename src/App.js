b78fb5165a20273f2c4c1cc561e9645390ff8b7e
shanmukheswarikondeti <eswarikondeti@gmail.com>
2024-02-19 13:00:08 +0530

    add footer component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import Header from './components/Header'
 import MainContent from './components/MainContent'
-
+import Footer from './components/Footer'
 
 import './App.css';
 
 const App=()=> (
-<>
-<Header/>
-<MainContent/>
-
-
-</>
+    <>
+        <Header/>
+        <MainContent/>
+        <Footer/>
+    </>
 )
 export default App;
