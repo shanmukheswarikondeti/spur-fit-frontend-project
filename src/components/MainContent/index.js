15cc69f0059ef0280eb54b70058e908e3972bdf8
shanmukheswarikondeti <eswarikondeti@gmail.com>
2024-02-19 07:20:58 +0530

    maincontent added the meetaheadcard component

--- src/components/MainContent/index.js
+++ src/components/MainContent/index.js
@@ -1,11 +1,13 @@
 import HomePage from '../HomePage'
 import EQBeatIQ from '../EQBeatIQ'
+import MeetAheadCard  from '../MeetAheadCard'
 
 import './index.css'
 const MainContent=()=>(
     <div className="app-container">
         <HomePage/>
         <EQBeatIQ/>
+        <MeetAheadCard/>
                
     </div>
 )
---
+++ src/components/MeetAheadCard/index.js
@@ -0,0 +1,23 @@
+import './index.css'
+
+const MeetAheadCard=()=>(
+    <div className="meet-ahead-container">
+        <div className="meet-ahead-header">
+            <p className="meet-ahead-top-text">Built out of frusstration</p>
+            <p className="meet-ahead-title">Meet the ahead app</p>
+        </div>
+        <div className="meet-ahead-body">
+            <img src="https://res.cloudinary.com/dlju0quch/image/upload/v1708255481/Screenshot_2024-02-18_165353_w0ubeq.png" className="meet-ahead-image" alt="meet ahead"/>
+            <p className="meet-ahead-content">
+                A personalized pocket coach that provides bite-sized, science-driven tools to boost emotional intelligence.
+                <br/>
+                <br/>
+                Think of it as a pocket cheerleader towards a better, more fulfilling.
+            </p>
+        </div>
+
+    </div>
+
+
+)
+export default MeetAheadCard;
